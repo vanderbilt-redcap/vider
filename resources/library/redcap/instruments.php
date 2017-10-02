e37192fdc7b600dae568930c40c31e363f95062e
oriapp <VICTR.Server.Admins@vanderbilt.edu>
2017-10-02 18:56:30 -0500

    Event support

--- resources/library/redcap/instruments.php
+++ resources/library/redcap/instruments.php
@@ -31,6 +31,30 @@ if (isset($_GET['form'])) {
     if (isset($instrument_names[$_GET['form']])) {
         $instrument_names = array($_GET['form'] => $instrument_names[$_GET['form']]); 
     }
+} else if (isset($_GET['event_id'])) {
+    $numEventId = 0;
+    if (is_numeric($_GET['event_id'])) {
+        // protect if outside project
+        if (REDCap::getEventNames(true, false, $_GET['event_id'])) {
+            $numEventId = $_GET['event_id'];
+        }
+    } else {
+        $numEventId = REDCap::getEventIdFromUniqueEvent($_GET['event_id']);
+    }
+    if (!$numEventId) {
+        $numEventId = 0;
+    }
+
+    $instrument_names = array();
+    if ($numEventId > 0) {
+        $instruments = array();
+        $sql = "SELECT form_name WHERE event_id = $numEventId;";
+        $q = db_query($sql);
+        while ($row = db_fetch_assoc($q)) {
+            $instruments[] = $row['form_name'];
+        }
+        $instrument_names = REDCap::getInstrumentNames($instruments);
+    }
 }
 
 //get the first element to adjust comma in json

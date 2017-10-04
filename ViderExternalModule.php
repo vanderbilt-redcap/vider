08077ed09792ceaff78178d717fa579f2a36b25f
oriapp <VICTR.Server.Admins@vanderbilt.edu>
2017-10-04 13:05:49 -0500

    Made into external module

---
+++ ViderExternalModule.php
@@ -0,0 +1,9 @@
+<?php
+namespace Vanderbilt\ViderExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+class ViderExternalModule extends AbstractExternalModule
+{
+}

af6c6dcadd82904d80527487e945e327277b4a18
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-08 16:57:58 -0600

    debug

--- bower_components/d3/d3.js
+++ bower_components/d3/d3.js
@@ -7543,12 +7543,12 @@
   function d3_scale_linearTickRange(domain, m) {
     if (m == null) m = 10;
     var extent = d3_scaleExtent(domain), span = extent[1] - extent[0], step = Math.pow(10, Math.floor(Math.log(span / m) / Math.LN10)), err = m / span * step;
-    console.log("extent 1: "+JSON.stringify(extent);
+    console.log("extent 1: "+JSON.stringify(extent));
     if (err <= .15) step *= 10; else if (err <= .35) step *= 5; else if (err <= .75) step *= 2;
     extent[0] = Math.ceil(extent[0] / step) * step;
     extent[1] = Math.floor(extent[1] / step) * step + step * .5;
     extent[2] = step;
-    console.log("extent 2: "+JSON.stringify(extent);
+    console.log("extent 2: "+JSON.stringify(extent));
     return extent;
   }
   function d3_scale_linearTicks(domain, m) {

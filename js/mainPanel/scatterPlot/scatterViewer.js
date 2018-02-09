b7a512851fa98b67bcb96bdf54613de1c99d3fcf
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-09 12:10:55 -0600

    scatter plot

--- js/mainPanel/scatterPlot/scatterViewer.js
+++ js/mainPanel/scatterPlot/scatterViewer.js
@@ -207,17 +207,17 @@ define(["jquery", "d3", "d3-tip", "colorbrewer", "filterData", "global"], functi
 
         // reformat for dates
         if (stratData[0].xValidation.match(/^date/)) {
-            $(svgElements).find(".x .tick text").each(function(idx, ob) {
+            $(svgElements).find(".tick").each(function(idx, ob) {
                 console.log("x "+idx);
-                var d1 = $(ob).html();
-                $(ob).html(getFormattedDate(d1));
+                // var d1 = $(ob).html();
+                // $(ob).html(getFormattedDate(d1));
             });
         }
         if (stratData[0].yValidation.match(/^date/)) {
-            $(svgElements).find(".y .tick text").each(function(idx, ob) {
+            $(svgElements).find(".tick").each(function(idx, ob) {
                 console.log("y "+idx);
-                var d1 = $(ob).html();
-                $(ob).html(getFormattedDate(d1));
+                // var d1 = $(ob).html();
+                // $(ob).html(getFormattedDate(d1));
             });
         }
 

d172feccf561618a90197b6c20ac1d891ae41d13
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-13 14:34:20 -0600

    color changes

--- js/variablePanel/viewVarPanel.js
+++ js/variablePanel/viewVarPanel.js
@@ -677,7 +677,7 @@ define(["require","d3","d3-tip","rugPlotHandler","categoryPlotHndlr","global","f
             else if(d.isSelected){
                 return "#8199C9";
             }
-            return d._children ? "#5C81CD" : d.children ? "#5C81CD" : i % 2  ? "#FFFFFF" : "#d3d3d3";
+            return d._children ? "#122f6a" : d.children ? "#122f6a" : i % 2  ? "#FFFFFF" : "#d3d3d3";
         }
 
 

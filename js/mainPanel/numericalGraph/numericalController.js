651b15fe543dc0b4dd6e86945f93365d38c9d461
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-13 14:43:27 -0600

    console.log

--- js/mainPanel/numericalGraph/numericalController.js
+++ js/mainPanel/numericalGraph/numericalController.js
@@ -24,7 +24,7 @@ define(["numericalView", "dataWrapper", "filterData","rebinning"],
          */
         var getNumericalCategories = function (data, obj, validation) {
             var max = Math.max.apply(Math, data);
-            console.log("min: "+Math.min.apply(Math, data));
+            console.log("min 1: "+Math.min.apply(Math, data));
             var domain = d3.scale.linear()
                 .domain([Math.min.apply(Math, data), max]);
 
@@ -124,6 +124,7 @@ define(["numericalView", "dataWrapper", "filterData","rebinning"],
                 self.categories = d3.values(rebinning.get(self.formName,self.varName))[0];
             }
             else{
+                console.log("min 2: "+Math.min.apply(Math, data));
                 var domain = d3.scale.linear()
                     .domain([Math.min.apply(Math, self.varData),
                         Math.max.apply(Math, self.varData)]);

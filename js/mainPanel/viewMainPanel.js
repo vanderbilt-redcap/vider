c08d1fb40505a7e07b4664a0ab2c65b1437f286f
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-28 10:08:55 -0600

    saveImage undone

--- js/mainPanel/viewMainPanel.js
+++ js/mainPanel/viewMainPanel.js
@@ -19,22 +19,6 @@ define(["require", "numHistogramHndlr", "nominalGraphHndlr", "dataWrapper",
         var instance = null;
         var varToD3Container = {};
 
-        ViewMainPanel.saveImage = function(svg) {
-            var serializer = new XMLSerializer();
-            var source = serializer.serializeToString(svg);
-
-            if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-            }
-            if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-            }
-
-            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-            var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-            window.location.href = url;
-        }
-
         /**
          * 1. Check if instance is null then throw error
          * 2. Calls the load ui related to this class

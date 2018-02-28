cf3a483585d4b7050fc64f782981c2a94dc9fc74
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-28 10:01:12 -0600

    Revert "svg save"
    This reverts commit 843a5498dab7c03cd4e2d059c0ac77e291b6fa81.

--- js/mainPanel/viewMainPanel.js
+++ js/mainPanel/viewMainPanel.js
@@ -200,11 +200,6 @@ define(["require", "numHistogramHndlr", "nominalGraphHndlr", "dataWrapper",
                             return !(el.formName === _formName && el.variableName === _variableName);
                         })
                         require("view").updateNewState(curStateData);
-                    })
-                    .append("span")
-                    .attr("class", "glyphicon glyphicon-floppy-save")
-                    .on("click", function() {
-                        saveImage(oprDiv.parent().parent().find(".graph-inner"));
                     });
 
                 //input
@@ -304,11 +299,6 @@ define(["require", "numHistogramHndlr", "nominalGraphHndlr", "dataWrapper",
                                     && el.var1 === var1 && el.var2 === var2 );
                         })
                         require("view").updateNewState(curStateData);
-                    })
-                    .append("span")
-                    .attr("class", "glyphicon glyphicon-floppy-save")
-                    .on("click", function() {
-                        saveImage(oprDiv.parent().parent().find(".graph-inner"));
                     });
 
                 //input
@@ -514,28 +504,6 @@ define(["require", "numHistogramHndlr", "nominalGraphHndlr", "dataWrapper",
                     inpDiv.remove();
                 });
 
-        }
-
-        function saveImage(svg) {
-            //get svg source.
-            var serializer = new XMLSerializer();
-            var source = serializer.serializeToString(svg);
-
-            //add name spaces.
-            if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-            }
-            if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-            }
-
-            //add xml declaration
-            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-            //convert svg source to URI data scheme.
-            var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-            window.location.href = url;
         }
 
         return ViewMainPanel.getInstance();

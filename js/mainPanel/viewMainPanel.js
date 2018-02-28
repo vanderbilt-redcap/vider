438da25d8597dab0b816c078b4a6dafc54178185
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-02-28 10:05:38 -0600

    saveImage

--- js/mainPanel/viewMainPanel.js
+++ js/mainPanel/viewMainPanel.js
@@ -506,5 +506,21 @@ define(["require", "numHistogramHndlr", "nominalGraphHndlr", "dataWrapper",
 
         }
 
+        function saveImage(svg) {
+            var serializer = new XMLSerializer();
+            var source = serializer.serializeToString(svg);
+
+            if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+            }
+            if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+            }
+
+            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+            var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+            window.location.href = url;
+        }
+
         return ViewMainPanel.getInstance();
     });
